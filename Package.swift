c3ffe7a2a1ea8b620cda6c32f1dab3ea6321186e
Tyler Jones <tyler@tylerjones.io>
2026-02-03 18:35:52 -0700

    Pin specific OpenCombine revision

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ if ProcessInfo.processInfo.environment["SPI_BUILDER"] == "1" {
 //swiftSettings.append(.define("SQLITE_HAS_CODEC"))
 //swiftSettings.append(.define("SQLCipher"))
 
-dependencies.append(.package(url: "https://github.com/OpenCombine/OpenCombine", branch: "master"))
+dependencies.append(.package(url: "https://github.com/OpenCombine/OpenCombine", revision: "1c6f02c7ed8140c0ba7a783aaddb6e0685a0037b"))
 
 dependencies
     .append(
